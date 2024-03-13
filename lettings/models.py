bad01fa62ee80bf8c9391df50b978f93d630fa1d
Hugo Huet-Leroy <hugohuetleroy@hugos-mac-studio.home>
2024-03-13 18:33:42 +0100

    docstring models

--- lettings/models.py
+++ lettings/models.py
@@ -3,6 +3,13 @@ from django.core.validators import MaxValueValidator, MinLengthValidator
 
 
 class Address(models.Model):
+    """
+    Model representing an address.
+
+    This model contains fields for the building number, street, city, state,
+    ZIP code, and country ISO code, and includes validators for specific fields
+    to ensure data integrity.
+    """    
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
@@ -11,6 +18,9 @@ class Address(models.Model):
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
     def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """        
         return f'{self.number} {self.street}'
     
     class Meta:
@@ -18,8 +28,17 @@ class Address(models.Model):
 
 
 class Letting(models.Model):
+    """
+    Model representing a letting.
+
+    This model links to the Address model and contains a title for the letting.
+    It represents properties available for rent.
+    """    
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
     def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """        
         return self.title
--- profiles/models.py
+++ profiles/models.py
@@ -3,8 +3,19 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    """
+    Model representing a user profile.
+
+    This model extends the base Django User model to include additional information
+    specific to the OC Lettings Site, such as the user's favorite city. The 'user'
+    field is a one-to-one link to the Django standard User model, ensuring that
+    each user can have only one profile and vice versa.
+    """    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_city = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """         
         return self.user.username
