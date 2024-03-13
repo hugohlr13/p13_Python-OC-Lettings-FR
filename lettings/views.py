b31d2e314fe88973c8edf80dfabcb038603efdbd
Hugo Huet-Leroy <hugohuetleroy@hugos-mac-studio.home>
2024-03-13 18:21:17 +0100

    templates urls admin

--- lettings/views.py
+++ lettings/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Letting
 
-# Create your views here.
+
+def index(request):
+    """
+    View function for the index page of lettings.
+
+    This view returns a list of all lettings to be displayed on the index page.
+    """
+    lettings_list = Letting.objects.all()
+    context = {'lettings_list': lettings_list}
+    return render(request, 'lettings/index.html', context)
+
+
+def letting(request, letting_id):
+    """
+    View function for displaying a specific letting.
+
+    Retrieves a letting by its ID and displays its details. If the letting is not found,
+    the request will result in a 404 error.
+
+    Args:
+        request: HttpRequest object.
+        letting_id (int): The ID of the letting to retrieve.
+
+    Returns:
+        HttpResponse object with the letting details.
+    """
+    letting = Letting.objects.get(id=letting_id)
+    context = {
+        'title': letting.title,
+        'address': letting.address,
+    }
+    return render(request, 'lettings/letting.html', context)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
+def index(request):
+    """
+    View for the index page of the Profiles application.
+
+    This view lists all profiles in the database. Each profile is associated with a user.
+    The profiles are passed to the 'profiles/index.html' template as part of the context.
+
+    :param request: HttpRequest object
+    :return: HttpResponse object with rendered index page
+    """
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles/index.html', context)
+
+
+def profile(request, username):
+    """
+    Detail view for a specific profile in the Profiles application.
+
+    This view retrieves a Profile based on the provided username. It then passes this
+    profile to the 'profiles/profile.html' template as part of the context.
+
+    :param request: HttpRequest object
+    :param username: Username associated with the Profile to be retrieved
+    :return: HttpResponse object with rendered profile page
+    """
+    profile = Profile.objects.get(user__username=username)
+    context = {'profile': profile}
+    return render(request, 'profiles/profile.html', context)
