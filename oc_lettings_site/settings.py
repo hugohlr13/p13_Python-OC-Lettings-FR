456571b9bf0795c6446f3ae205776023d25cf13b
Hugo Huet-Leroy <hugohuetleroy@hugos-mac-studio.home>
2024-03-20 10:42:53 +0100

    test_deploy_whitenoise

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://p13oclettingssite-b7502109c50e.herokuapp.com/']
 
 
 # Application definition
